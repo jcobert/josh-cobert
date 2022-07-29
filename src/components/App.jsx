5e550d970a38c8e1d615e8bac41a385b07a6e017
Josh Cobert <jdcobert@gmail.com>
2022-07-29 08:40:28 -0400

    add component skeletons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
-function App() {
+function App() {  
   return (
     <div className="flex-grow">
       <Header />
