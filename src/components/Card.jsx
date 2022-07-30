8a06877a0716bd6987207782fbf950a967efee51
Josh Cobert <jdcobert@gmail.com>
2022-07-30 12:58:28 -0400

    add technologies

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,10 @@ import { ChevronRightIcon } from "@heroicons/react/solid";
 import { NavLink } from "react-router-dom";
 
 function Card(props) {
+  // Objects.props.technologies.forEach(t => {
+
+  // });
+
   return (
     <div className="text-center">
       <div className="bg-slate-50 md:w-11/12 md:max-w-2xl mx-auto rounded-xl border border-slate-400 shadow-md">
@@ -11,27 +15,33 @@ function Card(props) {
           <h4>{props.title}</h4>
         </div>
         {/* Body */}
-        <div className="flex flex-col gap-y-6 text-left px-8 py-4">
+        <div className="flex flex-col gap-y-6 text-left px-8 pt-6 md:pt-8">
           {/* Description */}
-          <p>{props.descriptionShort}</p>
+          <p className="text-md sm:text-lg">{props.descriptionShort}</p>
           {/* Technologies */}
-          <div className="">
-            <h5 className="font-medium">Technologies</h5>
-            <ul className="list-disc list-outside pl-8 pt-2">
-              <li>React</li>
-              <li>Redux</li>
-              <li>Tailwind CSS</li>
-            </ul>
-          </div>
-        </div>
-        {/* CTA */}
-        <div className="bg-gray-50 hover:bg-theme-primary text-slate-700 hover:text-white font-medium w-96 sm:w-72 md:w-48 h-16 md:h-12 mx-auto my-8 rounded-md border border-theme-primary hover:border-white transition-all">
-          <NavLink to={"/projects"} className="flex justify-center h-full">
-            <p className="self-center">Check it out</p>
-            <div className="self-center">
-              <ChevronRightIcon className="w-6" />
+          <div className="flex flex-col gap-y-6 py-4">
+            {/* <h5 className="font-medium">Technologies</h5> */}
+            <div className="flex flex-wrap justify-around gap-x-8 gap-y-8">
+              {props.technologies.map((t, i) => (
+                <div
+                  key={i}
+                  className="flex flex-col items-center gap-x-2 gap-y-1 w-3/12"
+                >
+                  <i class={`${t.icon} text-4xl`}></i>
+                  <p className="text-center text-sm md:text-[1rem] whitespace-nowrap">{t.name}</p>
+                </div>
+              ))}
             </div>
-          </NavLink>
+            {/* CTA */}
+            <div className="bg-gray-50 hover:bg-theme-primary text-slate-700 hover:text-white font-medium w-full sm:w-6/12 md:w-4/12 h-16 md:h-12 mx-auto mt-4 mb-8 rounded-md border border-theme-primary hover:border-white transition-all">
+              <NavLink to={"/projects"} className="flex justify-center h-full">
+                <p className="self-center">Check it out</p>
+                <div className="self-center">
+                  <ChevronRightIcon className="w-6" />
+                </div>
+              </NavLink>
+            </div>
+          </div>
         </div>
       </div>
     </div>
