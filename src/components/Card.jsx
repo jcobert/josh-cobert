6858c97d8d50397af4ff2967ec838b62a7c480e1
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-07-30 18:52:20 -0400

    Create project card (#2)
    * structure markup
    
    * add project.json data
    
    * setup card props and styling
    
    * add project technologies list
    
    * add technologies
    
    * adjust title font size
    
    * add project image

---
+++ src/components/Card.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { ChevronRightIcon } from "@heroicons/react/solid";
+import { NavLink } from "react-router-dom";
+
+function Card(props) {
+  // Objects.props.technologies.forEach(t => {
+
+  // });
+
+  return (
+    <div className="text-center">
+      <div className="bg-slate-50 md:w-11/12 md:max-w-2xl mx-auto rounded-xl border border-slate-400 shadow-md">
+        {/* Title */}
+        <div className="py-4 font-semibold text-2xl md:text-2xl text-theme-primary bg-white rounded-xl rounded-b-none shadow-sm">
+          <h4>{props.title}</h4>
+        </div>
+        {/* Body */}
+        <div className="flex flex-col gap-y-6 text-left px-8 pt-6 md:pt-8">
+          {/* Description */}
+          <p className="text-md sm:text-lg">{props.descriptionShort}</p>
+          <div className="border shadow-sm w-11/12 mx-auto object-fill">
+            <img className="" src={props.img} alt="Project preview image" />
+          </div>
+          {/* Technologies */}
+          <div className="flex flex-col gap-y-6 py-4">
+            <h5 className="font-medium text-slate-700 text-center border border-x-0">
+              Technologies
+            </h5>
+            <div className="flex flex-wrap justify-around gap-x-8 gap-y-8">
+              {props.technologies.map((t, i) => (
+                <div
+                  key={i}
+                  className="flex flex-col items-center gap-x-2 gap-y-1 w-3/12"
+                >
+                  <i class={`${t.icon} text-4xl`}></i>
+                  <p className="text-center text-sm md:text-[1rem] whitespace-nowrap">
+                    {t.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+            {/* CTA */}
+            <div className="bg-gray-50 hover:bg-theme-primary text-slate-700 hover:text-white font-medium w-full sm:w-6/12 md:w-4/12 h-16 md:h-12 mx-auto mt-4 mb-8 rounded-md border border-theme-primary hover:border-white transition-all">
+              <NavLink to={"/projects"} className="flex justify-center h-full">
+                <p className="self-center">Check it out</p>
+                <div className="self-center">
+                  <ChevronRightIcon className="w-6" />
+                </div>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Heading from "../Heading";
+import Card from "../Card";
+import projects from "../../data/projects.json";
 
 function Home() {
   return (
     <div>
       <div className="lg:max-w-6xl lg:w-11/12 mx-auto mt-24 sm:mt-32 md:mt-40">
         <Heading />
+      </div>
+      <div>
+        {projects.map((p) => (
+          <Card 
+            key={p.id}
+            title={p.title}
+            descriptionShort={p.descriptionShort}
+            descriptionLong={p.descriptionLong}
+            details={p.details}
+            technologies={p.technologies}
+            img={p.img}
+          />
+        ))}
       </div>
     </div>
   );
