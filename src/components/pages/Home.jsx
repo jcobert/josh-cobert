be3af0d22882d6b98089cb1ad66a622892d30e42
Josh Cobert <jdcobert@gmail.com>
2022-07-30 22:10:15 -0400

    increase page content width

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,10 +6,10 @@ import projects from "../../data/projects.json";
 function Home() {
   return (
     <div>
-      <div className="lg:max-w-6xl lg:w-11/12 mx-auto mt-24 sm:mt-32 md:mt-40 mb-12">
+      <div className="lg:max-w-7xl lg:w-11/12 mx-auto mt-24 sm:mt-32 md:mt-40 mb-12">
         <Heading />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 lg:max-w-6xl gap-y-10 sm:gap-y-12 gap-x-6 mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 lg:max-w-7xl gap-y-10 sm:gap-y-12 gap-x-6 mx-auto">
         {projects.map((p) => (
           <Card 
             key={p.id}
