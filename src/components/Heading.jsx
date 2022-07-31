d6ed2475167910077648aa12b0169f6abf69a26d
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-07-31 14:02:25 -0400

    import page heading data to heading comp (#5)

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import headings from "../data/headings.json";
 
-function Heading() {
+const getHeading = (p) => {
+  return headings
+    .filter(function (heading) {
+      return heading.page === p;
+    })
+    .reduce(function (heading) {
+      return {
+        id: heading.id,
+        title: heading.title,
+        paragraph: heading.paragraph,
+      };
+    });
+};
+
+function Heading(props) {
   return (
     <div>
       <div className="flex flex-col gap-y-8 md:gap-y-10">
         <h1 className="font-flamenco text-theme-primary text-[2.5rem] sm:text-5xl md:text-6xl lg:text-7xl text-center lg:text-left">
-          Hey, I'm Josh Cobert
+          {props.title}
         </h1>
         <p className="text-lg w-10/12 mx-auto md:w-8/12 lg:w-6/12 lg:mx-0">
-          Thanks for paying me a visit. I am a software developer and creative
-          professional. I like long walks on the beach and refactoring code.
-          Please check out some of the projects I have worked on and feel free
-          to reach out if you'd like to collaborate!
+          {props.paragraph}
         </p>
       </div>
     </div>
   );
 }
 
-export default Heading;
+export { Heading as default, getHeading };
