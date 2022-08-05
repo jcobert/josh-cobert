39d64857f82eeaee702ca29d7b9a7ed293a62bc7
Josh Cobert <jdcobert@gmail.com>
2022-08-04 20:29:11 -0400

    add show all projects button

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Heading, { getHeading } from "../Heading";
 import { CardPreview } from "../Card";
 import projects from "../../data/projects.json";
-import { ChevronDoubleDownIcon } from "@heroicons/react/outline";
+import { Link } from "react-router-dom";
+import {
+  ChevronDoubleDownIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/outline";
 
 function Home() {
   const heading = getHeading("Home");
   const projectCards = [];
-  const featuredCount = 2;
+  const featuredCount = 3;
 
   projects.map((p) =>
     projectCards.push(
@@ -33,12 +37,23 @@ function Home() {
           paragraph={heading.paragraph}
         />
       </div>
-      <div className="flex flex-col items-center gap-y-6 text-xl text-theme-primary font-medium mt-12 md:mt-20 lg:mt-24 mb-16">
+      <div className="flex flex-col items-center gap-y-8 text-xl text-theme-primary font-medium mt-14 md:mt-20 lg:mt-24 mb-16">
         <h4 className="text-slate-600">Some of my projects...</h4>
         <ChevronDoubleDownIcon className="w-8 text-slate-400" />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 px-2 md:px-0 lg:max-w-7xl gap-y-14 sm:gap-y-16 gap-x-6 mx-auto">
         {projectCards.slice(0, featuredCount)}
+        {/* Show all button */}
+        <div className="px-8 self-center lg:col-start-2">
+          <div className="bg-theme-primary hover:bg-slate-500 text-white text-xl font-medium w-full sm:w-6/12 md:w-full h-20 mx-auto mt-4 mb-8 rounded-md border border-white hover:border-theme-primary transition-all">
+            <Link to={"/projects"} className="flex justify-center h-full">
+              <p className="self-center">All projects</p>
+              <div className="self-center">
+                <ChevronRightIcon className="w-6" />
+              </div>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
